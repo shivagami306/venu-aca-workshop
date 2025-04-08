d18e5ad7523b95e005149b73a4d2134b4aeae477
venuprabha.s <shivagami306@gmail.com>
2025-04-08 14:26:22 +0530

    ACR deployment

---
+++ aca/src/main/java/com/example/azurecontainerapp/AcaApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurecontainerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaApplication.class, args);
+	}
+
+}
